d773e91056ef93bda8fa47a11c01019f4b7de013
João Victor <jvaarantes@gmail.com>
2024-01-31 08:44:06 -0300

    Adicionar arquivos de projeto.

---
+++ AtividadePOO8/Pensao.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadePOO8
+{
+    internal class Pensao
+    {
+        public String Nome { get; set; }
+        public String Email { get; set; }
+        public int Quarto { get; set; }
+
+        public Pensao(String nome, String email, int quarto)
+        {
+            this.Nome = nome;
+            this.Email = email;
+            this.Quarto = quarto;
+        }
+        public override string ToString()
+        {
+            return $"{Quarto}: {Nome}, {Email}";
+        }
+
+    }
+}
---
+++ AtividadePOO8/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace AtividadePOO8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos quartos serão alugados? ");
+            int QuantidadeQuartos = Convert.ToInt32(Console.ReadLine());
+
+            Pensao[] Pensao = new Pensao[10];
+
+            for (int i = 0; i < QuantidadeQuartos; i++)
+            {
+                Console.WriteLine($"Aluguel #{i+1}");
+                Console.Write("Nome: ");
+                String Nome = Console.ReadLine();
+
+                Console.Write("E-mail: ");
+                String Email = Console.ReadLine();
+
+                Console.Write("Quarto: ");
+                int Quarto = Convert.ToInt32(Console.ReadLine());
+
+                Pensao[Quarto] = new Pensao(Nome, Email, Quarto);
+
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Quartos Ocupados:");
+
+            for (int i = 0; i < 10; i++) 
+            {
+                if (Pensao[i] != null )
+                {
+                    Console.WriteLine(Pensao[i].ToString());
+                }
+            }
+
+            Console.ReadKey();
+            Console.Beep();
+        }
+    }
+}
